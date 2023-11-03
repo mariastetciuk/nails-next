b5fdae9044a532a0ef0a4ab253275900db874ffe
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-11-04 00:29:33 +0200

    add base color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,29 @@ const config: Config = {
   ],
   theme: {
     extend: {
-     
+      colors: {
+        red: '#bb0a1e',
+        black: '#0A0A0A',
+        accent: '#FEE41F',
+        lemon: '#FFF5A5',
+        darkgray: '#565656',
+        graphite: '#4D4D4D',
+        disabled: '#A9A9A9',
+        darkyellow: '#B1A016',
+        lightyellow: '#FFFDED',
+        yellowshadow: '#DEDCCD',
+        lightgrey: '#F3F3F5'
+      },
+      fontFamily: {
+        namu: ['namu', 'sans-serif']
+      }
     },
-  },
+    screens: {
+      sm: '320px',
+      md: '768px',
+      lg: '1280px'
+    }
+    },
   plugins: [],
 }
 export default config
