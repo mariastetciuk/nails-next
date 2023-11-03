bd9880d020210ae5c8c0e1cc0ef4e88e6ff088aa
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-11-03 10:22:52 +0200

    first

---
+++ src/components/Header.tsx
@@ -0,0 +1,22 @@
+import { navLinks } from '@/data/navList';
+
+const Header = () => {
+
+  return (
+   
+      <header
+        className='fixed left-0 right-0 top-0 z-10 md:py-[15px] lg:py-5 bg-red-300 opacity-5'
+      >
+        <nav>
+          <ul>
+            {}
+          </ul>
+        </nav>
+        
+      </header>
+       
+  
+  );
+};
+
+export default Header;
---
+++ src/data/navList.ts
@@ -0,0 +1,13 @@
+type NavLinks = {
+    title: string;
+    href: string;
+  };
+  
+  export const navLinks: Array<NavLinks> = [
+    { title: 'Про нас', href: '#about-us' },
+    { title: 'Послуги', href: '#servises' },
+    { title: 'Прайс', href: '#prise' },
+    { title: 'Портфоліо', href: '#gallery' },
+    { title: 'Відгуки', href: '#revievs' },
+    { title: 'Контакти', href: '#contacts' }
+  ];
