9149e7ddac7fe65d5373beddcc1b58a2cb73f83f
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2024-01-13 15:41:59 +0200

    add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import './globals.css'
 import Header from '@/components/Header'
+import Head from 'next/head'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -17,6 +18,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ua">
+      <Head>
+        <link rel="icon" href='/favicon.png' />
+      </Head>
+      
       <body className={montserrat.className}>
         <Header />
         {children}</body>
