79faa7aa233fa183f396bfd4e00032f3db696093
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2025-02-09 18:06:43 +0200

    change link address

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,9 @@ import Link from "next/link";
 
 const cinzel = Cinzel({ subsets: ["latin"], weight: ["400"] });
 const address = "м. Івано-Франківськ, вул. Леся Курбаса 4/4";
-const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`;
+const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(
+  address
+)}`;
 
 const Footer = () => {
   return (
@@ -18,23 +20,38 @@ const Footer = () => {
           </p>
 
           <div>
-            <p className="font-bold mb-3 text-center md:text-left lg:mb-4 lg:w-[300px] lg:text-lg">Чекаємо Вас за адресою:</p>
-            <Link href={googleMapsUrl} target="_blank" rel="noopener noreferrer" className="block mb-4 md:mb-5 items-center text-white transition-all duration-300 hover:underline focus:underline">
-          вул. Леся Курбаса 4/4,
-          <span className="block">м. Івано-Франківськ</span>
-        </Link>
+            <p className="font-bold mb-3 text-center md:text-left lg:mb-4 lg:w-[300px] lg:text-lg">
+              Чекаємо Вас за адресою:
+            </p>
+            <Link
+              href="https://www.google.com/maps/place/Beautiful+Nails.+%D0%A1%D1%82%D1%83%D0%B4%D1%96%D1%8F+%D0%BC%D0%B0%D0%BD%D1%96%D0%BA%D1%8E%D1%80%D1%83+%D1%82%D0%B0+%D0%BF%D0%B5%D0%B4%D0%B8%D0%BA%D1%8E%D1%80%D1%83/@48.9210646,24.7125106,17z/data=!4m14!1m7!3m6!1s0x4730c184b9d02d8b:0x545e662ae1fac0e6!2zQmVhdXRpZnVsIE5haWxzLiDQodGC0YPQtNGW0Y8g0LzQsNC90ZbQutGO0YDRgyDRgtCwINC_0LXQtNC40LrRjtGA0YM!8m2!3d48.9210646!4d24.7125106!16s%2Fg%2F11vf3qw1ry!3m5!1s0x4730c184b9d02d8b:0x545e662ae1fac0e6!8m2!3d48.9210646!4d24.7125106!16s%2Fg%2F11vf3qw1ry?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="block mb-4 md:mb-5 items-center text-white transition-all duration-300 hover:underline focus:underline"
+            >
+              вул. Леся Курбаса 4/4,
+              <span className="block">м. Івано-Франківськ</span>
+            </Link>
           </div>
-          <p className="font-bold mb-3 text-center md:text-left lg:mb-4 lg:w-[300px] lg:text-lg">Графік роботи:</p>
+          <p className="font-bold mb-3 text-center md:text-left lg:mb-4 lg:w-[300px] lg:text-lg">
+            Графік роботи:
+          </p>
           <p className="mb-2">Вівторок - Неділя</p>
           <p>10:30 - 20:00</p>
         </div>
         <div className="mx-auto">
-          <h3 className=" font-bold mb-3 md:text-left md:text-lg lg:mb-4">Меню</h3>
+          <h3 className=" font-bold mb-3 md:text-left md:text-lg lg:mb-4">
+            Меню
+          </h3>
           <ul className="flex flex-wrap justify-center md:justify-start w-[80px] mx-auto gap-3 lg:gap-4">
-            {navLinks.map(({ href, title }) => (<li key={title}>
-              <Link  href={href} className="text-white md:text-lg border-b-2 border-transparent transition-all duration-300 hover:border-white  focus:border-white">
-                {title}
-              </Link>
+            {navLinks.map(({ href, title }) => (
+              <li key={title}>
+                <Link
+                  href={href}
+                  className="text-white md:text-lg border-b-2 border-transparent transition-all duration-300 hover:border-white  focus:border-white"
+                >
+                  {title}
+                </Link>
               </li>
             ))}
           </ul>
@@ -49,7 +66,10 @@ const Footer = () => {
                   target="_blank"
                   className="block text-white rounded-full p-2 border-2 border-transparent transition-all duration-300 hover:border-white  focus:border-white"
                 >
-                  <svg aria-label='icon sociale network' className="w-[32px] h-[32px] fill-current text-current md:w-[38px] md:h-[38px]">
+                  <svg
+                    aria-label="icon sociale network"
+                    className="w-[32px] h-[32px] fill-current text-current md:w-[38px] md:h-[38px]"
+                  >
                     <use href={href}></use>
                   </svg>
                 </Link>
@@ -60,9 +80,15 @@ const Footer = () => {
       </div>
       <div className="text-white text-center border-t-2 border-white pt-6 pb-[80px] md:text-lg lg:text-xl lg:py-6">
         <p className="inline">Розробка</p>
-       
-        <a href={'https://t.me/Mariia_Stetsiuk'} rel="noopener noreferrer" target='_blank' className=" cursor-pointer mx-2 border-b-2 border-white pt-3">Mariia Stetsiuk</a>
-        2023. 
+        <a
+          href={"https://t.me/Mariia_Stetsiuk"}
+          rel="noopener noreferrer"
+          target="_blank"
+          className=" cursor-pointer mx-2 border-b-2 border-white pt-3"
+        >
+          Mariia Stetsiuk
+        </a>
+        2023.
         <p className="md:inline md:ml-2">© Усі права захищені</p>
       </div>
     </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,8 +6,6 @@ import OrderServise from "./OrderServise";
 
 const cinzel = Cinzel({ subsets: ["latin"], weight: ["400"] });
 const vollkorn = Roboto_Serif({ subsets: ["latin"], weight: ["400"] });
-const address = "м. Івано-Франківськ, вул. Леся Курбаса 4/4";
-const googleMapsUrl = `https://www.google.com/maps/search/?api=1&query=${encodeURIComponent(address)}`;
 
 const Hero = () => {
   return (
@@ -24,55 +22,80 @@ const Hero = () => {
           cтудія манікюру та педикюру
         </h2>
         <div className="sm:px-60px md:flex md:justify-center sm:gap-[100px] lg:mb-5 lg:gap-10">
-<div className="flex flex-wrap mx-auto gap-3 max-w-[400px] sm:max-w-[450px] sm:text-xl md:max-w-[500px] md:text-2xl md:gap-6 lg:m-0 lg:border-r-2 lg:border-white lg:border-opacity-25">
-        <a href={"tel: +380989505917"} className="flex gap-6 md:gap-8 transition-all duration-300 hover:text-red hover:font-medium focus:text-red">
-          <svg aria-label="icon phone" className="w-[26px] h-[26px] text-center fill-current text-current md:w-[32px] md:h-[32px]">
-            <use href="/sprite.svg#icon-phone"></use>
-          </svg>
-          +38 (098) 9505917
-        </a>
-        <Link href={googleMapsUrl} target="_blank" rel="noopener noreferrer" className="flex gap-6 items-center text-white md:gap-8 transition-all duration-300 hover:text-red hover:font-medium focus:text-red">
-          <svg aria-label="icon map" className="w-[32px] h-[32px] fill-current text-current sm:w-[40px] sm:h[40px] md:w-[46px] md:h-[46px]">
-            <use href="/sprite.svg#icon-location"></use>
-          </svg>
-          м. Івано-Франківськ, вул. Леся Курбаса 4/4
-        </Link>
-        <div className="flex gap-6 text-white md:gap-8">
-          <svg aria-label="icon clock" className="w-[26px] h-[26px] fill-current text-current md:w-[32px] md:h-[32px]">
-            <use href="/sprite.svg#icon-clock"></use>
-          </svg>
-          <p>Вт-Нд: 10:30 - 20:00</p>
+          <div className="flex flex-wrap mx-auto gap-3 max-w-[400px] sm:max-w-[450px] sm:text-xl md:max-w-[500px] md:text-2xl md:gap-6 lg:m-0 lg:border-r-2 lg:border-white lg:border-opacity-25">
+            <a
+              href={"tel: +380989505917"}
+              className="flex gap-6 md:gap-8 transition-all duration-300 hover:text-red hover:font-medium focus:text-red"
+            >
+              <svg
+                aria-label="icon phone"
+                className="w-[26px] h-[26px] text-center fill-current text-current md:w-[32px] md:h-[32px]"
+              >
+                <use href="/sprite.svg#icon-phone"></use>
+              </svg>
+              +38 (098) 9505917
+            </a>
+            <Link
+              href="https://www.google.com/maps/place/Beautiful+Nails.+%D0%A1%D1%82%D1%83%D0%B4%D1%96%D1%8F+%D0%BC%D0%B0%D0%BD%D1%96%D0%BA%D1%8E%D1%80%D1%83+%D1%82%D0%B0+%D0%BF%D0%B5%D0%B4%D0%B8%D0%BA%D1%8E%D1%80%D1%83/@48.9210646,24.7125106,17z/data=!4m14!1m7!3m6!1s0x4730c184b9d02d8b:0x545e662ae1fac0e6!2zQmVhdXRpZnVsIE5haWxzLiDQodGC0YPQtNGW0Y8g0LzQsNC90ZbQutGO0YDRgyDRgtCwINC_0LXQtNC40LrRjtGA0YM!8m2!3d48.9210646!4d24.7125106!16s%2Fg%2F11vf3qw1ry!3m5!1s0x4730c184b9d02d8b:0x545e662ae1fac0e6!8m2!3d48.9210646!4d24.7125106!16s%2Fg%2F11vf3qw1ry?entry=ttu&g_ep=EgoyMDI1MDIwNS4xIKXMDSoASAFQAw%3D%3D"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="flex gap-6 items-center text-white md:gap-8 transition-all duration-300 hover:text-red hover:font-medium focus:text-red"
+            >
+              <svg
+                aria-label="icon map"
+                className="w-[32px] h-[32px] fill-current text-current sm:w-[40px] sm:h[40px] md:w-[46px] md:h-[46px]"
+              >
+                <use href="/sprite.svg#icon-location"></use>
+              </svg>
+              м. Івано-Франківськ, вул. Леся Курбаса 4/4
+            </Link>
+            <div className="flex gap-6 text-white md:gap-8">
+              <svg
+                aria-label="icon clock"
+                className="w-[26px] h-[26px] fill-current text-current md:w-[32px] md:h-[32px]"
+              >
+                <use href="/sprite.svg#icon-clock"></use>
+              </svg>
+              <p>Вт-Нд: 10:30 - 20:00</p>
+            </div>
+            <Link
+              href={"mailto:beautifulnails.if@gmail.com"}
+              className="flex gap-6 text-white md:gap-8 transition-all duration-300 hover:text-red hover:font-medium focus:text-red"
+            >
+              <svg
+                aria-label="icon email"
+                className="w-[26px] h-[26px] fill-current text-current md:w-[32px] md:h-[32px]"
+              >
+                <use href="/sprite.svg#icon-envelope"></use>
+              </svg>
+              <p>beautifulnails.if@gmail.com</p>
+            </Link>
+          </div>
+          <div className="hidden lg:block bg-white bg-opacity-20 py-5 w-[400px] h-[260px] rounded-3xl lg:w-[450px]">
+            <OrderServise />
+          </div>
         </div>
-        <Link
-          href={"mailto:beautifulnails.if@gmail.com"}
-          className="flex gap-6 text-white md:gap-8 transition-all duration-300 hover:text-red hover:font-medium focus:text-red"
-        >
-          <svg aria-label="icon email" className="w-[26px] h-[26px] fill-current text-current md:w-[32px] md:h-[32px]">
-            <use href="/sprite.svg#icon-envelope"></use>
-          </svg>
-          <p>beautifulnails.if@gmail.com</p>
-        </Link>
-
-       
-      </div>
-    <div className="hidden lg:block bg-white bg-opacity-20 py-5 w-[400px] h-[260px] rounded-3xl lg:w-[450px]">
-   <OrderServise />
-    </div>
-  
-    </div>
-    <ul className="flex py-6 justify-center items-center w-[100%] gap-5 md:gap-7">
-          {socialeList.map(({link, href}) => <li key={href}>
-            <Link href={link}  rel="noopener noreferrer" target='_blank' className="block text-white rounded-full p-2 border-2 border-transparent scale-100 transition-all duration-300 hover:border-white hover:text-red  focus:border-white focus:text-red">
-             <svg aria-label="icon sociale network" className="w-[32px] h-[32px] fill-current text-current sm:w-[38px] sm:h-[38px] md:w-[42px] md:h-[42px]">
-         <use href={href}></use>
-             </svg>
-           </Link>
-          </li>
-          )}
+        <ul className="flex py-6 justify-center items-center w-[100%] gap-5 md:gap-7">
+          {socialeList.map(({ link, href }) => (
+            <li key={href}>
+              <Link
+                href={link}
+                rel="noopener noreferrer"
+                target="_blank"
+                className="block text-white rounded-full p-2 border-2 border-transparent scale-100 transition-all duration-300 hover:border-white hover:text-red  focus:border-white focus:text-red"
+              >
+                <svg
+                  aria-label="icon sociale network"
+                  className="w-[32px] h-[32px] fill-current text-current sm:w-[38px] sm:h-[38px] md:w-[42px] md:h-[42px]"
+                >
+                  <use href={href}></use>
+                </svg>
+              </Link>
+            </li>
+          ))}
         </ul>
         <FixedBtn />
       </div>
-     
     </section>
   );
 };
