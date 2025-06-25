852cccf45a721a2193ab035834b4e76a946cf621
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2025-06-25 18:17:28 +0300

    change price blok

--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -19,16 +19,16 @@ const Price = () => {
           whileInView={{ opacity: 1, x: 0 }}
           transition={{ ease: "easeOut", duration: 1 }}
         >
-          <h2 className="relative text-center uppercase leading-[30px] font-extrabold text-red text-[84px] sm:leading-[33px] sm:text-[90px] text-opacity-70 mb-[80px] md:w-[400px] md:mx-auto md:leading-[59px] md:text-[130px] md:mb-[100px] lg:text-[200px] lg:mb-[90px] lg:w-[560px] lg:pt-[40px] lg:leading-[75px]">
-            наш{" "}
+          <h2 className="relative text-center uppercase leading-[31px] font-extrabold text-red text-[84px] sm:leading-[35px] sm:text-[90px] text-opacity-70 mb-[80px] md:w-[500px] md:mx-auto md:leading-[59px] md:text-[130px] md:mb-[100px] lg:text-[200px] lg:mb-[90px] lg:w-[700px] lg:pt-[40px] lg:leading-[75px]">
+            наші{" "}
             <motion.div
               viewport={{ once: true }}
-              initial={{ opacity: 0, x: 250 }}
+              initial={{ opacity: 0, x: 350 }}
               whileInView={{ opacity: 1, x: 0 }}
               transition={{ ease: "easeOut", duration: 1 }}
             >
               <span className=" font-bold left-1/2 -translate-x-1/2 text-black text-[45px] sm:text-[50px] md:text-[85px] text-opacity-1 md:-bottom-[102px] lg:bottom-0 lg:text-[110px]">
-                прайс
+                послуги
               </span>
             </motion.div>
           </h2>
@@ -38,7 +38,7 @@ const Price = () => {
           {price.map(({ id, title, src, description, price }) => (
             <li
               key={id}
-              className={`${isPriceItem && id > 4 ? 'hidden__item' : ''} mx-auto px-3 relative pt-[80px] text-center bg-lightgrey rounded-3xl w-[250px] h-[300px] sm:px-4 lg:w-[350px] lg:pt-[90px]`}
+              className={`${isPriceItem && id > 4 ? 'hidden__item' : ''} mx-auto px-3 relative pt-[80px] text-center bg-lightgrey rounded-3xl w-[250px] h-[250px] sm:px-4 lg:w-[350px] lg:pt-[90px] bg:h-[300px]`}
             >
               <Image
                 src={src}
@@ -47,11 +47,11 @@ const Price = () => {
                 height={130}
                 className=" absolute left-1/2 -translate-x-1/2 -top-[60px] border-4 rounded-full border-white"
               />
-              <h2 className="md:text-lg font-medium mb-2 lg:mb-3">{title}</h2>
-              <p className=" text-sm lg:text-base">{description}</p>
-              <p className="absolute left-1/2 -translate-x-1/2 bottom-4 border-2 border-red mx-auto bg-white w-[150px] py-2 rounded-2xl">
+              <h2 className="md:text-lg font-medium mb-5 lg:mb-3 bg:text-2xl bg:mb-5">{title}</h2>
+              <p className=" text-sm lg:text-base bg:text-lg">{description}</p>
+              {/* <p className="absolute left-1/2 -translate-x-1/2 bottom-4 border-2 border-red mx-auto bg-white w-[150px] py-2 rounded-2xl">
                 {price}
-              </p>
+              </p> */}
             </li>
           ))}
         </ul>
